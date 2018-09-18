7247806cb162710d16baa44c106d3f0fe5d38459
James <jgbentley10@gmail.com>
2018-09-18 18:40:00 +0100

    Make monitoring machines work

--- NetworkMonitor.cpp
+++ NetworkMonitor.cpp
@@ -6,6 +6,8 @@
 #include <arpa/inet.h>
 #include <iostream>
 #include <vector>
+#include <sstream>
+#include <map>
 
 using namespace std;
 
@@ -63,41 +65,130 @@ class SocketMonitor {
 	char ipstr[INET6_ADDRSTRLEN];
 };
 
+// Utilities Functions
+vector<string> split(const string& s, char delimiter) {
+   vector<string> tokens;
+   string token;
+   stringstream tokenStream(s);
+   while (getline(tokenStream, token, delimiter)) {
+      tokens.push_back(token);
+   }
+   return tokens;
+}
+
 // this will be our parent class for both masters and slaves
 class Machine {
     public:
-    Machine();
-    ~Machine();
-    private:
+    Machine() {};
+    ~Machine() { cout << "Die Machine!" << endl; };
+    int parseStartup(vector<string>* msg) {
+        if(msg->size() != 3) {
+            cout<<"Machine startup message includes "<<msg->size()<<" parameters, must include exactly 3 to start a machine.";
+            return -1;
+        }
+        machineID = (*msg)[1];
+        version = "not yet reported.";
+        fps = "not yet reported.";
+        return 1; 
+    }
+
+    int parseHeartbeat(vector<string>* msg) {
+        if(msg->size() != 4) {
+            cout<<"Machine heartbeat message includes "<<msg->size()<<" parameters, must include exactly 4 to register as a machine heartbeat.";
+            return -1;
+        }
+        if(machineID == (*msg)[1]) {
+            fps = (*msg)[3];
+            version = (*msg)[2];
+        }
+        return 1; 
+    }
+
+    string getVersion() { return version; };
+    string getMachineID() { return machineID; };
+    string getFps() { return fps; };
+
+    protected:
     string machineID;
     string version;
-    float fps;
-};
-
-// This represents a master machine
-class Master : public Machine {
-    public:
-    Master() {};
-    ~Master() {};
-    private:
-};
-
-// This represents a slave machine
-class Slave : public Machine {
-    public:
-    Slave() {};
-    ~Slave() {};
-    private:
+    string fps;
+    string sessionToJoin;
 };
 
 // This represents a session
 class Session {
     public:
+    Session() {};
+    ~Session() {};
+    void parseStartup(vector<string>* msg);
     private:
     string name;
-    Master* creator;
-    vector<Slave* > slaves;
+    Machine* creator;
+    vector<string > permittedMachineIDs;
+    vector<Machine* > slaves;
 };
+
+vector<Machine* > machines;
+vector<Session* > sessions;
+
+int onStartupMessageRecieved(char * buffer) {
+    // First we'll split our string on ur expected delimiter:
+    vector<string> message = split(buffer, '|');
+    // for(int i = 0; i < message.size(); i++) {
+    //     cout<<message[i]<<endl;
+    // }
+    if(message.size() > 0) {
+        string messageType = message[0];
+        if(messageType == "SESSION2") {
+            // it's a master startup moment.
+            // Session* session = new Session();
+            // session->parseStartup(&message);
+            // sessions.push_back(session);
+            // master
+        } else if(messageType == "MACHINE") {
+            // it's a machine startup moment.
+            Machine* machine = new Machine();
+            machine->parseStartup(&message);
+            for(int i = 0; i < machines.size(); i++) {
+                if(machines[i]->getMachineID() == machine->getMachineID()) {
+                    // We this machine has already started up!
+                    delete machine;
+                    return 1;
+                }
+            }
+            machines.push_back(machine);
+        } else {
+            // It's an unknown message that we can ignore.
+            cout<<"Recieved Message: "<<buffer<<" has a malformed type. Valid types are: MACHINE or SESSION2"<<endl;
+            return -1;
+        }
+    } else {
+        cout<<"Recieved message: " << buffer << " is malformed and contains no delimiters"<<endl;
+        return -1;
+    }
+    return 1;
+}
+
+int onHeartbeatMessageRecieved(char * buffer) {
+    vector<string> message = split(buffer, '|');
+    if(message.size() > 0) {
+        string messageType = message[0];
+        if(messageType == "MACHINESTATUS") {
+            // it's a machine heartbeat moment.
+            for(int i = 0; i < machines.size(); i++) {
+                machines[i]->parseHeartbeat(&message);
+            }
+        } else {
+            // It's an unknown message that we can ignore.
+            cout<<"Recieved message: "<< buffer << " has a malformed type. Valid type is: MACHINESTATUS"<<endl;
+            return -1;
+        }
+    } else {
+        cout<<"Recieved message: " << buffer << " is malformed and contains no delimiters"<<endl;
+        return -1;
+    }
+    return 1;
+}
 
 int main() {
 
@@ -110,12 +201,19 @@ int main() {
 
     while(1) {
         if(m4.getMessageRecieved(b1) > 0) {
-            cout<<b1<<endl;
+            // cout<<b1<<endl;            
+            onHeartbeatMessageRecieved(b1);
             memset(b1, 0, sizeof(b1));
         }
         if(m6.getMessageRecieved(b2) > 0) {
-            cout<<b2<<endl;
+            // cout<<b2<<endl;
+            onStartupMessageRecieved(b2);
             memset(b2, 0, sizeof(b2));
+            cout<<machines.size()<<endl;
+            for(int i = 0; i < machines.size(); i++) {
+                cout<<" Machine: "<<machines[i]->getMachineID()<<" version: "<<machines[i]->getVersion()<<" fps: "<<machines[i]->getFps()<<endl;
+            }
+            cout<<endl;
         }
     }
     return 0;
